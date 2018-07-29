bbdafff8217daeda1e2412f027be26cc417cab41
jon Opacich <jonopacich@gmail.com>
2018-07-28 19:55:20 -0500

    deploy

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,27 @@
 let mysql = require('mysql');
 
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'todo_db',
-    port:3306
-  });
-   
-  connection.connect(err => {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-   
-    console.log('connected as id ' + connection.threadId);
-  });
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  //Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'todo_db',
+    port: 3306
+  })
+}
+connection.connect(err => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+})
 module.exports = connection;
